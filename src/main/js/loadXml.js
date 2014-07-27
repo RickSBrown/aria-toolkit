cdbc923d8caa2a5c889a6ee69411cd15fdf6fb17
ricksbrown73@gmail.com <ricksbrown73@gmail.com@1c8eaeeb-3888-3043-36a8-e20a9d32e640>
2014-07-27 05:02:25 +0000

    Port to AMD.

--- src/main/js/loadXml.js
+++ src/main/js/loadXml.js
@@ -1,18 +1,23 @@
-/**
- * A REALLY basic xml loader.
- */
-function loadXml(url)
-{
-	var xhr;
-	if(window.XMLHttpRequest)
+define(function(){
+	/**
+	 * A REALLY basic xml loader.
+	 * @param {string} url The location of the XML to load.
+	 */
+	function loadXml(url)
 	{
-		xhr = new XMLHttpRequest();
+		var xhr;
+		if(window.XMLHttpRequest)
+		{
+			xhr = new XMLHttpRequest();
+		}
+		else
+		{
+			xhr = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xhr.open("GET", url, false);
+		xhr.send();
+		return xhr.responseXML;
 	}
-	else
-	{
-		xhr = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xhr.open("GET", url, false);
-	xhr.send();
-	return xhr.responseXML;
-}
+
+	return loadXml;
+});
--- src/main/js/xpath.js
+++ src/main/js/xpath.js
@@ -1,75 +1,79 @@
-/**
- * @param {string} expr The xpath expression
- * @param {boolean} firstMatch set to true to return a single node only
- * @param doc The XML dom to query
- * @return {Element|Array} The result of the xpath expression
- */
-function query(expr, firstMatch, doc)
-{
-	var next, result, match, nsResolver, i, len;
-	if(doc.ownerDocument)
+define(function(){
+	/**
+	 * @param {string} expr The xpath expression
+	 * @param {boolean} firstMatch set to true to return a single node only
+	 * @param doc The XML dom to query
+	 * @return {Element|Array} The result of the xpath expression
+	 */
+	function query(expr, firstMatch, doc)
 	{
-		doc = doc.ownerDocument;
-	}
-	if(doc.evaluate)
-	{
-		nsResolver = document.createNSResolver(doc.documentElement);
-		if(firstMatch)
+		var next, result, match, nsResolver, i, len;
+		if(doc.ownerDocument)
 		{
-			result = (doc.evaluate(expr, doc, nsResolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null)).singleNodeValue;
+			doc = doc.ownerDocument;
 		}
-		else
+		if(doc.evaluate)
 		{
-			result = [];
-			match = doc.evaluate(expr, doc, nsResolver, XPathResult.ANY_TYPE, null);
-			while((next = match.iterateNext()))
+			nsResolver = document.createNSResolver(doc.documentElement);
+			if(firstMatch)
 			{
-				result[result.length] = next;
+				result = (doc.evaluate(expr, doc, nsResolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null)).singleNodeValue;
 			}
-		}
-	}
-	else
-	{
-		ieNamespaceResolver(doc);
-		if(firstMatch)
-		{
-			result = doc.selectSingleNode(expr);
-		}
-		else 
-		{
-			result = [];
-			match = doc.selectNodes(expr);
-			if(match)
+			else
 			{
-				for(i=0, len=match.length; i<len; i++)
+				result = [];
+				match = doc.evaluate(expr, doc, nsResolver, XPathResult.ANY_TYPE, null);
+				while((next = match.iterateNext()))
 				{
-					result[result.length] = match[i];
+					result[result.length] = next;
 				}
 			}
 		}
-	}
-	return result;
-}
-
-function ieNamespaceResolver(doc)
-{
-	var i, next, namespaces, attributes, namespaceRe;
-	if(doc.getProperty("SelectionNamespaces"))
-	{
-		namespaces = "";
-		attributes = doc.documentElement.attributes;
-		namespaceRe = ieNamespaceResolver.nsRe || (ieNamespaceResolver.nsRe = /^xmlns/);
-		for(i=attributes.length - 1; i>=0; i--)
+		else
 		{
-			next = attributes[i];
-			if(namespaceRe.test(next.name))
+			ieNamespaceResolver(doc);
+			if(firstMatch)
 			{
-				namespaces  += next.name + "='" + next.value + "' ";
+				result = doc.selectSingleNode(expr);
+			}
+			else
+			{
+				result = [];
+				match = doc.selectNodes(expr);
+				if(match)
+				{
+					for(i=0, len=match.length; i<len; i++)
+					{
+						result[result.length] = match[i];
+					}
+				}
 			}
 		}
-		if(namespaces)
+		return result;
+	}
+
+	function ieNamespaceResolver(doc)
+	{
+		var i, next, namespaces, attributes, namespaceRe;
+		if(doc.getProperty("SelectionNamespaces"))
 		{
-			doc.setProperty("SelectionNamespaces", namespaces);
+			namespaces = "";
+			attributes = doc.documentElement.attributes;
+			namespaceRe = ieNamespaceResolver.nsRe || (ieNamespaceResolver.nsRe = /^xmlns/);
+			for(i=attributes.length - 1; i>=0; i--)
+			{
+				next = attributes[i];
+				if(namespaceRe.test(next.name))
+				{
+					namespaces  += next.name + "='" + next.value + "' ";
+				}
+			}
+			if(namespaces)
+			{
+				doc.setProperty("SelectionNamespaces", namespaces);
+			}
 		}
 	}
-}
+
+	return query;
+});
