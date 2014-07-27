a190f1f2c427390d53896b925e74251803ae710d
ricksbrown73@gmail.com <ricksbrown73@gmail.com@1c8eaeeb-3888-3043-36a8-e20a9d32e640>
2014-07-27 08:56:04 +0000

    Cleaning up buildscripts after AMD port.

--- build.js
+++ build.js
@@ -1,11 +1,12 @@
 /*
- * This is the build configuration for the aria core layer file.
+ * This is the build configuration for the aria layer files.
+ * Properties will be expanded.
  * This is consumed by r.js which is initiated from the main Ant build.
  */
 ({
-	baseUrl: "build/js_debug",
+	baseUrl: "build/${js.debug.dir.name}",
 	name: '../../libs/requirejs/almond',
-	out: 'build/js_debug/aria-layer.js',
+	out: 'build/${js.debug.dir.name}/${layer.name.js}',
 	//preserveLicenseComments: false,
 	optimize: "none",//closure | none
 	normalizeDirDefines: "all",
@@ -14,5 +15,5 @@
 		startFile: 'src/main/js/start.frag',
 		endFile: 'src/main/js/end.frag'
 	},
-	include: ['aria']
+	include: ['${module.name}']
 })
